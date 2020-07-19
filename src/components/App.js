00dec28f360007a23e29248e2130339c6fd4fb9e
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-07-20 00:25:36 +0200

    Play mode

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Mic from './../assets/images/mic.jpg';
 import back from './../assets/images/back.jpg';
 import Buttons from './buttons';
+import Tomorrow from './../assets/songs/Tomorrow.mp3';
+import StressOut from './../assets/songs/StressedOut.mp3';
+export class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <img src={back} alt="background" className="background"></img>
+    this.state = {
+      SongSrc: [{Tomorrow}, {StressOut}, {Tomorrow}, {StressOut}, {Tomorrow}],
+      SongIndex: 0,
+      PlayPause: true,
+    };
+  }
 
-      <div className="app-wrapper">
-        <div className="main">
-          <img src={Mic} alt="songImage" className="SongImg"></img>
+  Play = () => {
+    let song = document.getElementById('Song');
+    if (this.state.PlayPause) {
+      song.play();
+      this.setState({PlayPause: false});
+      console.log('ON');
+    } else {
+      song.pause();
+      this.setState({PlayPause: true});
+      console.log('Off');
+    }
+  };
+
+  Prev = () => {
+    this.setState({SongIndex: this.state.SongIndex - 1});
+    if (this.state.SongIndex < 0) {
+      this.setState({SongIndex: 5});
+    }
+    document.getElementById('Song').src = this.state.SongSrc[
+      this.state.SongIndex
+    ];
+    this.Play();
+  };
+  Next = () => {
+    this.setState({SongIndex: this.state.SongIndex + 1});
+    if (this.state.SongIndex > 5) {
+      this.setState({SongIndex: 0});
+    }
+    document.getElementById('Song').src = this.state.SongSrc[
+      this.state.SongIndex
+    ];
+    this.Play();
+  };
+
+  BarProcess = () => {
+    const BarProg = document.querySelector('#SongProc');
+    BarProg.max = document.getElementById('Song').duration;
+    BarProg.value = document.getElementById('Song').currentTime;
+    setInterval(this.BarProcess, 700);
+  };
+  render() {
+    return (
+      <div className="App">
+        <img src={back} alt="background" className="background"></img>
+        <audio loop src={StressOut} id="Song"></audio>
+        <div className="app-wrapper">
+          <div className="main">
+            <img src={Mic} alt="songImage" className="SongImg"></img>
+          </div>
+          <div className="container">
+            <h2>song title</h2>
+          </div>
+          <Buttons
+            Play={this.Play}
+            PlayPause={this.state.PlayPause}
+            Next={this.Next}
+            Prev={this.Prev}
+          />
         </div>
-        <div className="container">
-          <h2>song title</h2>
-        </div>
-        <Buttons />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import './App.css';
-function Buttons() {
-  return (
-    <div className="container ">
-      <button className="clicks">
-        <i className="fas fa-arrow-alt-circle-left click"></i>
-      </button>
-      <button className="clicks">
-        <i className="fas fa-play-circle click"></i>
-      </button>
-      <button className="clicks">
-        <i className="fas fa-stop-circle click"></i>
-      </button>
-      <button className="clicks">
-        <i className="fas fa-arrow-alt-circle-right click"></i>
-      </button>
-    </div>
-  );
+class Buttons extends React.Component {
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+    console.log(event.target.value);
+  };
+  render = () => {
+    return (
+      <div className="container">
+        <button className="clicks" onClick={this.props.Prev}>
+          <i className="fas fa-arrow-alt-circle-left click"></i>
+        </button>
+        <button className="clicks" onClick={this.props.Play}>
+          <span>
+            <i
+              class={this.props.PlayPause ? 'fa fa-play-circle' : 'fa fa-stop'}
+            ></i>
+          </span>
+        </button>
+        <button className="clicks" onClick={this.props.Next}>
+          <i className="fas fa-arrow-alt-circle-right click"></i>
+        </button>
+        <input
+          type="range"
+          min="0"
+          max=""
+          onChange={this.handleChange}
+          defaultValue="0"
+          className="SongProc"
+          id="SongProc"
+        />
+      </div>
+    );
+  };
 }
 
 export default Buttons;
